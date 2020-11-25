c41c1a881e28cf02b02f36873fbaac3d10331278
Wouter Hobers <wouter.hobers+github@gmail.com>
2020-11-25 22:33:54 +0100

    Refactoring.

--- main.c
+++ main.c
@@ -4,23 +4,26 @@
 #include "cpu.h"
 
 
-int main(int argc, char* argv[]) {
-  SDL_Window* window = NULL;
-
+static int main_init(SDL_Window** window) {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     SDL_Log("SDL_Init: %s\n", SDL_GetError());
-    return 1;
+    return -1;
   }
 
-  window = SDL_CreateWindow("twatwa", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
-  if (window == NULL) {
+  *window = SDL_CreateWindow("twatwa", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+  if (*window == NULL) {
     SDL_Log("SDL_CreateWindow: %s\n", SDL_GetError());
-    return 2;
+    return -1;
   }
-  
+
   clock_init();
   cpu_init();
 
+  return 0;
+}
+
+
+static void main_eventloop(SDL_Window* window) {
   for (;;) {
     SDL_Event event;
   
@@ -33,9 +36,24 @@ int main(int argc, char* argv[]) {
       break;
     }
   }
- 
+}
+
+
+static void main_finit(SDL_Window* window) {
   SDL_DestroyWindow(window);
   SDL_Quit();
+}
+
+
+int main(int argc, char* argv[]) {
+  SDL_Window* window;
+
+  if (main_init(&window) != 0) {
+    return 1;
+  }
+
+  main_eventloop(window);
+  main_finit(window);
 
   return 0;
 }
