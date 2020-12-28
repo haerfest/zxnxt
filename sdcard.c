2cb5cbb7ae820bed7cc56f94ae99ba82e3d52951
Wouter Hobers <wouter.hobers+github@gmail.com>
2020-12-28 13:48:56 +0100

    Fix SD card states.

--- sdcard.c
+++ sdcard.c
@@ -42,8 +42,7 @@ typedef enum {
 } error_t;
 
 typedef enum {
-  E_STATE_INACTIVE = 0,
-  E_STATE_IDLE,
+  E_STATE_IDLE = 0,
   E_STATE_READY,
   E_STATE_IDENTIFICATION,
   E_STATE_STANDBY,
@@ -51,7 +50,8 @@ typedef enum {
   E_STATE_SENDING_DATA,
   E_STATE_RECEIVE_DATA,
   E_STATE_PROGRAMMING,
-  E_STATE_DISCONNECT
+  E_STATE_DISCONNECT,
+  E_STATE_INACTIVE
 } state_t;
   
 typedef struct {
@@ -199,18 +199,15 @@ static void sdcard_handle_command(sdcard_nr_t n) {
         return;
 
       case E_CMD_SEND_OP_COND:
-        self[n].state              = E_STATE_IDLE;
         self[n].response_buffer[0] = 0x00;  /* Need to clear idle bit. */
         self[n].response_length    = 1;
         return;
  
       case E_CMD_SEND_IF_COND:
-        self[n].state = E_STATE_IDLE;
         self[n].error = E_ERROR_ILLEGAL_COMMAND;
         return;
 
       case E_CMD_SEND_CSD:
-        self[n].state = E_STATE_IDLE;
         self[n].response_buffer[0] = 0x00;   /* R1. */
         self[n].response_buffer[1] = 0xFE;   /* Start data token. */
         
@@ -265,6 +262,7 @@ static void sdcard_handle_command(sdcard_nr_t n) {
           return;
         }
 
+        self[n].state                                         = E_STATE_SENDING_DATA;
         self[n].response_buffer[0]                            = 0x00;  /* R1. */
         self[n].response_buffer[1]                            = 0xFE;  /* Start data token. */
         self[n].response_buffer[2 + self[n].block_length + 0] = 0x00;  /* CRC. */
@@ -275,7 +273,7 @@ static void sdcard_handle_command(sdcard_nr_t n) {
       case E_CMD_APP_SEND_OP_COND:
         if (self[n].in_app_cmd) {
           self[n].in_app_cmd         = 0;
-          self[n].state              = E_STATE_IDLE;
+          self[n].state              = E_STATE_READY;
           self[n].response_buffer[0] = 0x00;  /* Need to indicate busy. */
           self[n].response_length    = 1;
           return;
@@ -287,7 +285,7 @@ static void sdcard_handle_command(sdcard_nr_t n) {
         return;
 
       case E_CMD_READ_OCR:
-        self[n].state              = E_STATE_IDLE;
+        self[n].state              = E_STATE_SENDING_DATA;
         self[n].response_buffer[0] = 0x00;                               /* R1. */
         self[n].response_buffer[1] = 0x80 | (1 - self[n].is_sdsc) << 6;  /* Powered-up + CCS. */
         self[n].response_buffer[2] = 0;
