f33daa391ee9be326ea88f07252a818659912512
Wouter Hobers <wouter.hobers+github@gmail.com>
2021-01-13 00:32:12 +0100

    Refactoring.

--- log.h
+++ log.h
@@ -4,10 +4,15 @@
 
 #include <stdio.h>
 
-
+#ifdef SILENT
+#define log_err(...)
+#define log_inf(...)
+#define log_wrn(...)
+#else
 #define log_err(...) fprintf(stderr, __VA_ARGS__)
 #define log_inf(...) fprintf(stderr, __VA_ARGS__)
 #define log_wrn(...) fprintf(stderr, __VA_ARGS__)
+#endif
 
 #ifdef DEBUG
 #define log_dbg(...) fprintf(stderr, __VA_ARGS__)
