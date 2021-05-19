7813d45b578d1dc35573babc51f47fb7ec59b22e
Wouter Hobers <wouter.hobers+github@gmail.com>
2021-05-19 20:34:37 +0200

    Rudimentary text mode implemented.

--- tilemap.c
+++ tilemap.c
@@ -6,17 +6,18 @@
 
 
 typedef struct {
-  u8_t*  sram_bank5;
-  u8_t   default_attribute;
-  u16_t  definitions_base_address;
-  u16_t  tilemap_base_address;
-  u8_t   transparency_index;
-  int    is_enabled;
-  int    use_80x32;
-  int    use_default_attribute;
-  int    use_secondary_palette;
-  int    use_512_tiles;
-  int    force_tilemap_over_ula;
+  u8_t*     bank5;
+  u8_t      default_attribute;
+  u16_t     definitions_base_address;
+  u16_t     tilemap_base_address;
+  u8_t      transparency_index;
+  int       is_enabled;
+  int       use_80x32;
+  int       use_default_attribute;
+  int       use_text_mode;
+  int       use_512_tiles;
+  int       force_tilemap_over_ula;
+  palette_t palette;
 } tilemap_t;
 
 
@@ -26,9 +27,9 @@ static tilemap_t self;
 int tilemap_init(u8_t* sram) {
   memset(&self, 0, sizeof(self));
 
-  self.sram_bank5               = &sram[MEMORY_RAM_OFFSET_ZX_SPECTRUM_RAM + 5 * 16 * 1024];
-  self.definitions_base_address = 0x4C00;
-  self.tilemap_base_address     = 0x6C00;
+  self.bank5                    = &sram[MEMORY_RAM_OFFSET_ZX_SPECTRUM_RAM + 5 * 16 * 1024];
+  self.definitions_base_address = 0x0C00;
+  self.tilemap_base_address     = 0x2C00;
   self.transparency_index       = 0x0F;
 
   return 0;
@@ -43,9 +44,11 @@ void tilemap_tilemap_control_write(u8_t value) {
   self.is_enabled             = value & 0x80;
   self.use_80x32              = value & 0x40;
   self.use_default_attribute  = value & 0x20;
-  self.use_secondary_palette  = value & 0x10;
+  self.use_text_mode          = value & 0x08;
   self.use_512_tiles          = value & 0x02;
   self.force_tilemap_over_ula = value & 0x01;
+
+  self.palette = (value & 0x10) ? E_PALETTE_TILEMAP_SECOND : E_PALETTE_TILEMAP_FIRST;
 }
 
 
@@ -70,38 +73,38 @@ void tilemap_transparency_index_write(u8_t value) {
 
 
 void tilemap_tick(u32_t row, u32_t column, int* is_transparent, u16_t* rgba) {
-  u8_t  tile;
-  u16_t map_address;
-  u16_t map_stride;
-  u16_t tile_address;
-  u8_t  palette_offset;
-  u8_t  palette_index;
-  palette_entry_t colour;
 
   if (!self.is_enabled) {
     *is_transparent = 1;
     return;
   }
 
-  if (!self.use_80x32) {
-    column /= 2;
+  // assume 80x32, not default attribute
+
+  u8_t  map_row    = row    / 8;
+  u8_t  map_column = column / 8;
+  u16_t map_offset = self.tilemap_base_address + (map_row * 80 + map_column) * 2;
+  u8_t  tile       = self.bank5[map_offset + 0];
+  u8_t  attribute  = self.bank5[map_offset + 1];
+
+  if (self.use_512_tiles) {
+    tile |= (attribute & 0x01) << 8;
   }
 
-  map_stride     = (self.use_80x32 ? 80 : 40) * (self.use_default_attribute ? 1 : 2);
-  map_address    = self.tilemap_base_address + row * map_stride + (column / 8);
-  tile           = self.sram_bank5[map_address];
-  tile_address   = self.definitions_base_address + tile * 32;
-  palette_offset = (self.use_default_attribute ? self.default_attribute : memory_read(tile_address + 1)) & 0xF0;
-  palette_index  = self.sram_bank5[tile_address + column / 2];
+  u8_t  def_row        = row    % 8;
+  u8_t  def_column     = column % 8;
+  u16_t def_offset     = self.use_text_mode
+    ? (self.definitions_base_address + (tile *  8 + def_row))
+    : (self.definitions_base_address + (tile * 32 + def_row * 4 + def_column / 2));
+  u8_t  pattern        = self.bank5[def_offset];
 
-  /* High or low nibble. */
-  if (column & 0x01) {
-    palette_index &= 0x0F;
-  } else {
-    palette_index >>= 4;
-  }
+  u8_t  palette_index  = self.use_text_mode
+    ? ((pattern & (0x80 >> def_column)) ? 1 : 0)
+    : ((def_column & 0x01) ? (pattern & 0x0F) : (pattern >> 4));
+    
+  u8_t  palette_offset = attribute & (self.use_text_mode ? 0xFE : 0xF0);
 
-  colour          = palette_read_rgb(E_PALETTE_ULA_FIRST, palette_offset | palette_index);
-  *rgba           = colour.red << 12 | colour.green << 8 | colour.blue << 4;
-  *is_transparent = 0;
+  palette_entry_t colour = palette_read_rgb(self.palette, palette_offset | palette_index);
+  *rgba                  = colour.red << 12 | colour.green << 8 | colour.blue << 4;
+  *is_transparent        = 0;
 }
