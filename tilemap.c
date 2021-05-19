916a2d73d0461c81b6a5e4489900dc8dc4a3dbf4
Wouter Hobers <wouter.hobers+github@gmail.com>
2021-05-19 17:18:47 +0200

    Force tilemap to use bank 5.

--- tilemap.c
+++ tilemap.c
@@ -1,10 +1,12 @@
 #include <string.h>
 #include "log.h"
+#include "memory.h"
+#include "palette.h"
 #include "tilemap.h"
 
 
 typedef struct {
-  u8_t*  sram;
+  u8_t*  sram_bank5;
   u8_t   default_attribute;
   u16_t  definitions_base_address;
   u16_t  tilemap_base_address;
@@ -24,7 +26,7 @@ static tilemap_t self;
 int tilemap_init(u8_t* sram) {
   memset(&self, 0, sizeof(self));
 
-  self.sram                     = sram;
+  self.sram_bank5               = &sram[MEMORY_RAM_OFFSET_ZX_SPECTRUM_RAM + 5 * 16 * 1024];
   self.definitions_base_address = 0x4C00;
   self.tilemap_base_address     = 0x6C00;
   self.transparency_index       = 0x0F;
@@ -68,11 +70,38 @@ void tilemap_transparency_index_write(u8_t value) {
 
 
 void tilemap_tick(u32_t row, u32_t column, int* is_transparent, u16_t* rgba) {
+  u8_t  tile;
+  u16_t map_address;
+  u16_t map_stride;
+  u16_t tile_address;
+  u8_t  palette_offset;
+  u8_t  palette_index;
+  palette_entry_t colour;
+
   if (!self.is_enabled) {
     *is_transparent = 1;
     return;
   }
 
-  *rgba           = 0xF000;
+  if (!self.use_80x32) {
+    column /= 2;
+  }
+
+  map_stride     = (self.use_80x32 ? 80 : 40) * (self.use_default_attribute ? 1 : 2);
+  map_address    = self.tilemap_base_address + row * map_stride + (column / 8);
+  tile           = self.sram_bank5[map_address];
+  tile_address   = self.definitions_base_address + tile * 32;
+  palette_offset = (self.use_default_attribute ? self.default_attribute : memory_read(tile_address + 1)) & 0xF0;
+  palette_index  = self.sram_bank5[tile_address + column / 2];
+
+  /* High or low nibble. */
+  if (column & 0x01) {
+    palette_index &= 0x0F;
+  } else {
+    palette_index >>= 4;
+  }
+
+  colour          = palette_read_rgb(E_PALETTE_ULA_FIRST, palette_offset | palette_index);
+  *rgba           = colour.red << 12 | colour.green << 8 | colour.blue << 4;
   *is_transparent = 0;
 }
