dd6f2867f383ce34e75a21acdaa8a93f7e8b4415
Wouter Hobers <wouter.hobers+github@gmail.com>
2022-09-06 23:26:07 +0200

    Small optimisation.

--- copper.c
+++ copper.c
@@ -30,14 +30,26 @@ typedef struct instruction_t {
 } instruction_t;
 
 
+typedef enum opcode_t {
+  E_OPCODE_NOOP,
+  E_OPCODE_WAIT,
+  E_OPCODE_MOVE
+} opcode_t;
+
+
 typedef struct copper_t {
   u16_t         cpc;      /** 0 - 1023 */
   u16_t         address;  /** 0 - 2047 */
-  instruction_t instruction[1024];
+  instruction_t instructions[1024];
   u8_t          cached;
   int           is_running;
   int           do_reset_pc_on_irq;
   int           do_move_wait_one_cycle;
+  opcode_t      opcode;
+  u32_t         wait_row;
+  u32_t         wait_column;
+  u8_t          move_reg;
+  u8_t          move_value;
 } copper_t;
 
 
@@ -62,7 +74,7 @@ void copper_reset(reset_t reset) {
 
 
 void copper_data_8bit_write(u8_t value) {
-  ((u8_t*) copper.instruction)[copper.address] = value;
+  ((u8_t*) copper.instructions)[copper.address] = value;
   copper.address = (copper.address + 1) & 0x7FF;
 }
 
@@ -70,8 +82,8 @@ void copper_data_8bit_write(u8_t value) {
 void copper_data_16bit_write(u8_t value)  {
   if (copper.address & 1) {
     const u16_t even = copper.address - 1;
-    copper.instruction[even].msb = copper.cached;
-    copper.instruction[even].lsb = value;
+    copper.instructions[even].msb = copper.cached;
+    copper.instructions[even].lsb = value;
   } else {
     copper.cached = value;
   }
@@ -86,7 +98,25 @@ void copper_address_write(u8_t value)  {
 
 
 u16_t copper_program_get(u16_t address) {
-  return (copper.instruction[address].msb << 8) | copper.instruction[address].lsb;
+  return (copper.instructions[address].msb << 8) | copper.instructions[address].lsb;
+}
+
+
+static void copper_decode(void) {
+  const u16_t instruction = (copper.instructions[copper.cpc].msb << 8) | copper.instructions[copper.cpc].lsb;
+    
+  if (instruction == 0x0000) {
+    copper.opcode = E_OPCODE_NOOP;
+  } else if (instruction & 0x8000) {
+    copper.opcode      = E_OPCODE_WAIT;
+    copper.wait_row    = instruction & 0x01FF;
+    copper.wait_column = (instruction & 0x7E00) >> 5;  /* (>> 6) * 2 b/c sub-pixel beam */
+  } else {      
+    copper.opcode                 = E_OPCODE_MOVE;
+    copper.move_reg               = (instruction & 0x7F00) >> 8;
+    copper.move_value             = instruction & 0x00FF;
+    copper.do_move_wait_one_cycle = 1;
+  }
 }
 
 
@@ -117,39 +147,45 @@ void copper_control_write(u8_t value) {
       copper.is_running             = 1;
       break;
   }
+
+  if (copper.is_running) {
+    copper_decode();
+  }
 }
 
 
 void copper_tick(u32_t beam_row, u32_t beam_column, int ticks_28mhz) {
-  int    tick;
-  u16_t instruction;
-
   if (!copper.is_running) {
     return;
   }
 
-  for (tick = 0; tick < ticks_28mhz; tick++) {
-    instruction = (copper.instruction[copper.cpc].msb << 8) | copper.instruction[copper.cpc].lsb;
+  for (int tick = 0; tick < ticks_28mhz; tick++) {
+    switch (copper.opcode) {
+      case E_OPCODE_NOOP:
+        /* NOOP: 1 cycle. */
+        copper.cpc = (copper.cpc + 1) & 0x3FF;
+        copper_decode();
+        break;
 
-    if (instruction == 0x0000) {
-      /* NOOP: 1 cycle. */
-      copper.cpc = (copper.cpc + 1) & 0x3FF;
-    } else if (instruction & 0x8000) {
-      /* WAIT: 1 cycle. */
-      const u32_t wait_row    = instruction & 0x01FF;
-      const u32_t wait_column = (instruction & 0x7E00) >> 5;  /* (>> 6) * 2 b/c sub-pixel beam */
-      if (beam_row == wait_row && beam_column >= wait_column) {
-        copper.cpc = (copper.cpc + 1) & 0x3FF;
-      }
-    } else {      
-      /* MOVE: 2 cycles. */
-      if (copper.do_move_wait_one_cycle) {
-        const u8_t reg   = (instruction & 0x7F00) >> 8;
-        const u8_t value = instruction & 0x00FF;
-        nextreg_write_internal(reg, value);
-        copper.cpc = (copper.cpc + 1) & 0x3FF;
-      }
-      copper.do_move_wait_one_cycle = !copper.do_move_wait_one_cycle;
+      case E_OPCODE_WAIT:
+        /* WAIT: 1 cycle. */
+        if (beam_row == copper.wait_row && beam_column >= copper.wait_column) {
+          copper.cpc = (copper.cpc + 1) & 0x3FF;
+          copper_decode();
+        } else {
+          /* No need to waste cycles if the beam is not where we need it. */
+          return;
+        }
+        break;
+
+      case E_OPCODE_MOVE:
+        /* MOVE: 2 cycles. */
+        if (copper.do_move_wait_one_cycle) {
+          nextreg_write_internal(copper.move_reg, copper.move_value);
+          copper.cpc  = (copper.cpc + 1) & 0x3FF;
+          copper_decode();
+        }
+        copper.do_move_wait_one_cycle = !copper.do_move_wait_one_cycle;
     }
   }
 }
@@ -157,7 +193,7 @@ void copper_tick(u32_t beam_row, u32_t beam_column, int ticks_28mhz) {
 
 void copper_irq(void) {
   if (copper.do_reset_pc_on_irq) {
-    copper.cpc                    = 0;
-    copper.do_move_wait_one_cycle = 0;
+    copper.cpc = 0;
+    copper_decode();
   }
 }
